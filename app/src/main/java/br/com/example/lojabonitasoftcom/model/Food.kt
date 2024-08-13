ff2fb197bfd06955adeaffe1c3bd98ea84e9d326
Victor_Santos <126700032+viiiga@users.noreply.github.com>
2024-08-13 03:30:53 -0300

    Quase finalização, fase de polimento, com implementação da navbar e topappbar.

---
+++ app/src/main/java/br/com/example/lojabonitasoftcom/model/Food.kt
@@ -0,0 +1,8 @@
+package br.com.example.lojabonitasoftcom.model
+
+data class Food (
+    val imgFood: Int? = null,
+    val foodName: String? = null,
+    val foodDescription: String? = null,
+    val price: String? = null
+)
